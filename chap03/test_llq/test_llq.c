fb6fa0ea08e55677fce540fa4c834ec6645be1ed
YOUNGJIN JOO <neoelec@gmail.com>
2024-09-17 16:19:01 +0900

    (b-algorithm): chap03/test_llq: added
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ chap03/test_llq/test_llq.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <container/llq.h>
+
+struct LLQ_Entry {
+    struct DLL_Node node;
+    const char *data;
+};
+
+static struct LLQ_Entry *LLQ_EnqueueEntry(struct LLQ *queue, const char *data)
+{
+    struct LLQ_Entry *entry = malloc(sizeof(*entry));
+
+    entry->data = data;
+    LLQ_Enqueue(queue, &entry->node);
+
+    return entry;
+}
+
+static struct LLQ_Entry *LLQ_DequeueEntry(struct LLQ *queue)
+{
+    struct DLL_Node *node = LLQ_Dequeue(queue);
+
+    return CONTAINER_OF(node, struct LLQ_Entry, node);
+}
+
+static struct LLQ *LLQ_CreateQueue(void)
+{
+    struct LLQ *queue = malloc(sizeof(*queue));
+
+    LLQ_Init(queue);
+
+    return queue;
+}
+
+static void LLQ_DestroyQueue(struct LLQ *queue)
+{
+    while (!LLQ_IsEmpty(queue))
+        free(LLQ_DequeueEntry(queue));
+
+    free(queue);
+}
+
+int main(int argc, char *argv[])
+{
+    struct LLQ_Entry *entry;
+    struct LLQ *queue = LLQ_CreateQueue();
+
+    LLQ_EnqueueEntry(queue, "abc");
+    LLQ_EnqueueEntry(queue, "def");
+    LLQ_EnqueueEntry(queue, "efg");
+    LLQ_EnqueueEntry(queue, "hij");
+
+    printf("Count: %zu\n\n", queue->count);
+
+    while (!LLQ_IsEmpty(queue)) {
+        entry = LLQ_DequeueEntry(queue);
+        printf("Dequeue: %s\n", entry->data);
+    }
+
+    LLQ_DestroyQueue(queue);
+
+    return 0;
+}
