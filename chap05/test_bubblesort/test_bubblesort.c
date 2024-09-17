522ee2aa4dc7e4f4fce70e79ecf55a116a8d6dbf
YOUNGJIN JOO <neoelec@gmail.com>
2024-09-17 16:30:18 +0900

    (b-algorithm): chap05/test_bubblesort: added
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

---
+++ chap05/test_bubblesort/test_bubblesort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include <algorithm/bubblesort.h>
+
+static int Compare(const void *_a, const void *_b)
+{
+    int a = *(const int *)_a;
+    int b = *(const int *)_b;
+
+    return a - b;
+}
+
+int main(int argc, char *argv[])
+{
+    int data[] = { 6, 4, 2, 3, 1, 5 };
+    void *ptr_arr[NR_ELEM(data)];
+    size_t i;
+
+    for (i = 0; i < NR_ELEM(data); i++)
+        ptr_arr[i] = &data[i];
+
+    BubbleSort(ptr_arr, NR_ELEM(data), Compare);
+
+    for (i = 0; i < NR_ELEM(data); i++)
+        printf("%d ", *(int *)ptr_arr[i]);
+
+    printf("\n");
+
+    return 0;
+}
